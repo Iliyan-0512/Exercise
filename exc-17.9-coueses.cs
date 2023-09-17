64807bb5fc305099cf7db9d0ecd97cf9a445ba7e
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2023-09-17 19:32:15 +0300

    Add files via upload

---
+++ exc-17.9-coueses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] word = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> letters = new Dictionary<char, int>();
+            foreach (var letter in word)
+            {
+                if (letter != ' ')
+                {
+                    if (!letters.ContainsKey(letter))
+                    {
+                        letters[letter] = 0;
+                    }
+                    letters[letter]++;
+                }
+
+
+            }
+            foreach (var kvpLetter in letters)
+            {
+                Console.WriteLine($"{kvpLetter.Key} -> {kvpLetter.Value}");
+            }
+        }
+    }
+}
