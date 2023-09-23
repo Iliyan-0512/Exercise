696eb284fd203b0d887d8da6a2a7c86b1fce0ed7
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2023-09-23 21:34:27 +0300

    Add files via upload

---
+++ Replace Repeating Chars.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char previousChart = text[0];
+            Console.Write(previousChart);
+            for (int i = 1; i < text.Length; i++)
+            {
+                char currentChar= text[i];
+                if (previousChart != currentChar)
+                {
+                    previousChart = currentChar;
+                    Console.Write(previousChart);
+                }
+            }
+        }
+
+    }
+}
