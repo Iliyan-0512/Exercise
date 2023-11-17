512b2763fa52b4bca697b82c678c48cff0b9acbd
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2023-11-17 22:16:58 +0200

    Add files via upload

---
+++ Count Chars in a String.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+
+        Dictionary<char, int> charCounter = new Dictionary<char, int>();
+
+        foreach (char c in input)
+        {
+            if (c != ' ')
+            {
+                if (charCounter.ContainsKey(c))
+                {
+                    charCounter[c]++;
+                }
+                else
+                {
+                    charCounter[c] = 1;
+                }
+            }
+        }
+
+        Console.WriteLine("Character occurrences:");
+        foreach (var kvp in charCounter)
+        {
+            Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+        }
+    }
+}
