8b7678425b31d2d1a5a40caf001b7feace9fd6cb
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2023-11-18 15:04:46 +0200

    Add files via upload

---
+++ 02. A Miner Task.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> list =Console.ReadLine().Split(" ").ToList();
+            Dictionary<string, int> items = new Dictionary<string, int>();
+            string resources = Console.ReadLine();
+            while (resources != "stop")
+            {
+                int quantities = int.Parse(Console.ReadLine());
+                if (!items.ContainsKey(resources))
+                {
+                    items[resources] = 0;
+                }
+                items[resources] += quantities;
+                resources = Console.ReadLine();
+            }
+            foreach (var currRecou in items)
+            {
+                Console.WriteLine($"{currRecou.Key} -> {currRecou.Value}");
+            }
+
+
+
+
+        }
+    }
+}
