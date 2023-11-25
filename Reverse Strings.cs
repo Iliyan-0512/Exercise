5273413c05f2491714b5304131541e630aedb45e
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2023-11-25 22:00:41 +0200

    Add files via upload

---
+++ Reverse Strings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var input=Console.ReadLine();
+           var stack=new Stack<char>();
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+            while (stack.Count > 0);
+            {
+                Console.WriteLine(stack.Count>0);
+            }
+            Console.WriteLine();
+        }
+    }
+}
