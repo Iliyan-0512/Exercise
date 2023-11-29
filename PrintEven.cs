9435e843bc8aed8cdcf4edc167819f4bcc8410e6
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2023-11-29 23:32:04 +0200

    no complete

---
+++ PrintEven.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>();
+            foreach (int element in arr)
+            {
+                queue.Enqueue(element);
+            }
+            foreach (int element in queue)
+            {
+                queue.Dequeue();
+            }
+        }
+    }
+}
