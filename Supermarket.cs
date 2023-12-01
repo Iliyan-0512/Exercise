fc9e40f0a229fdf69e5abc0d7a67222c81236bb2
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2023-12-01 20:52:10 +0200

    Add files via upload

---
+++ Supermarket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Supermarket
+    {
+        static void Main(string[] args)
+        {
+            
+            Queue<string> queue = new Queue<string>();
+            while (true)
+            {
+                string commnad = Console.ReadLine();
+                
+                if (commnad == "End")
+                {
+                    Console.WriteLine($"{queue.Count} people remaining.");
+                    break;
+                }
+                else if (commnad == "Paid")
+                {
+                    foreach (string line in queue)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    queue.Clear();
+                }
+                else
+                {
+                    queue.Enqueue(commnad);
+                }
+                
+
+            }
+        }
+    }
+}
