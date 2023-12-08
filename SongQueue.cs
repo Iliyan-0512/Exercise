f7f0d241573334af3fc678ffdecf5a74edd6d86d
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2023-12-08 22:34:42 +0200

    Add files via upload

---
+++ SongQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class fastfood
+{
+    static void Main()
+    {
+        string[] songs=Console.ReadLine().Split(",").ToArray();
+        Queue<string> queue = new  Queue<string>();
+        string command = Console.ReadLine();
+        while (queue.Count>0) 
+        {
+            if (command=="Play")
+            {
+                queue.Dequeue();
+            }
+            else if (command=="Add")
+            {
+                if (!queue.Contains(songs[0]))
+                {
+                    queue.Enqueue(songs[0]);
+                }
+                else
+                {
+                    Console.WriteLine($"{songs[0]} is already contained!");
+                }
+            }
+            else if(command=="Show")
+            {
+                foreach (string s in songs)
+                {
+                    Console.WriteLine(s + ", ");
+                }
+            }
+        }
+    }
+}
