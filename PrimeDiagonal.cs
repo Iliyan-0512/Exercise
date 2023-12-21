a16fcf5dfed23951c036f61ca2b57b8a6c10437b
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2023-12-21 18:11:14 +0200

    Add files via upload

---
+++ PrimeDiagonal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the size of the matrix from the console
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0; // Initialize the sum of the diagonal elements
+
+            // Iterate over each row of the matrix
+            for (int i = 0; i < n; i++)
+            {
+                // Read the current row into an array of integers
+                string[] row = Console.ReadLine().Split(' ');
+                // Add the diagonal element to the sum
+                // Diagonal elements have equal row and column indices
+                sum += int.Parse(row[i]);
+            }
+
+            // Output the sum of the diagonal elements
+            Console.WriteLine(sum);
+        }
+    }
+}
+
