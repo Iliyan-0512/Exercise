f0fa9aa8d188512a2ca67f5f95e71d34f534e3c4
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-01-07 16:22:05 +0200

    Add files via upload

---
+++ ArrayRotation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Arrayrotation
+    {
+        static void Main(string[] args)
+        {
+            int[] number=Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int count=int.Parse(Console.ReadLine());
+            count = count % number.Length;
+
+            for (int i = 1; i <= count; i++)
+            {
+                int temp = number[0];
+                for (int j = 1; j<number.Length ; j++)
+                {
+                    number[j-1] = number[j];
+                }
+                number[number.Length-1]=temp;
+            }
+
+            Console.WriteLine(string.Join(" ", number));
+
+        }
+    }
+}
