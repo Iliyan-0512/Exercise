89edfbe834f541ea5aca920b1977798106a6e17c
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-02-20 14:32:36 +0200

    Add files via upload

---
+++ types.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //float myFloatValue = 10;
+            ////int myIntValue = myFloatValue;
+            //int myIntValue = (int)myFloatValue;
+            //Console.WriteLine("myIntValue is " + myIntValue);
+            //int myInt = 36;
+            //float myFloat = 16.4F;
+            //myFloat = myInt + myFloat;
+            //Console.WriteLine(myFloat);
+
+         //   int myInt = 10;
+         //   byte myByte = (byte)myInt;
+         //   double myDouble = (double)myByte;
+            
+         //   string myString = "false";
+         //   ;
+         //   myString = myInt.ToString();
+            
+         //   short myShort = (short)myInt;
+         //   char myChar = 'x';
+         //;
+         //   long myLong = (long)myInt;
+         //   decimal myDecimal = (decimal)myLong;
+         //   myString = myString + myInt +
+         //   myByte + myDouble + myChar;
+            Console.ReadKey();
+            
+        }
+        //public int MyMethod(bool add3)
+        //{
+        //    int value = 12;
+        //    if (add3)
+        //        value += 3;
+        //    else
+        //        value -= 2;
+        //    return value;
+        //}
+        
+    }
+}
