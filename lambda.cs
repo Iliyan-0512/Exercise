b3b9b5db41eb50e0a6bc4c7cbbe14eee47026220
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-02-25 23:48:45 +0200

    Add files via upload

---
+++ lambda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+            .Split(new string[] { ", " },
+            StringSplitOptions.RemoveEmptyEntries)
+            .Select(n => int.Parse(n))
+            .Where(n => n % 2 == 0)
+            .OrderBy(n => n)
+            .ToArray();
+            string result = string.Join(", ", numbers);
+            Console.WriteLine(result);
+
+        }
+    }
+}
