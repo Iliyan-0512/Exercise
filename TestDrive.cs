ec331df0abd5abb28e838c95e739430ae9c200b1
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-02-28 20:22:19 +0200

    Add files via upload

---
+++ TestDrive.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomInt = random.Next();
+            Console.WriteLine(randomInt);
+
+            int zeroNine = random.Next(10);
+            Console.WriteLine(zeroNine);
+
+            int dieRow = random.Next(7);
+
+            double randomDouble = random.NextDouble();
+
+            Console.WriteLine(randomDouble * 100);
+
+            Console.WriteLine((float)randomDouble * 100F);
+            Console.WriteLine((decimal)randomDouble * 100M);
+
+            int zeroOrOne = random.Next(2);
+            bool coinFlip = Convert.ToBoolean(zeroOrOne);
+            Console.WriteLine(coinFlip);
+
+
+        }
+
+
+
+    }
+}
