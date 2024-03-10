2451b43f73d00b477e122b0005b760ba4337bbc8
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-03-10 15:38:05 +0200

    Add files via upload

---
+++ SumNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class SumNumber
+    {
+        static void Main(string[] args)
+        {
+           
+            string input = Console.ReadLine();
+
+            
+            int[] numbers = input.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(n => int.Parse(n))
+                                 .ToArray();
+
+            
+            int count = numbers.Count();
+
+           
+            int sum = numbers.Sum();
+
+            Console.WriteLine(count);
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
