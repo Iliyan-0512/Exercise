4d6b0bd5cd434eb4c9968bd35843a36688f01011
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-03-11 10:39:24 +0200

    Add files via upload

---
+++ CountUppercaseWords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class 	CountUppercaseWords
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> checker = n => n[0] == n.ToUpper()[0];
+            string[] words = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Where(w => checker(w))
+            .ToArray();
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
