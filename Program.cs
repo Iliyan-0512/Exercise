3310d0fb756a17b59c0ee9030d0ba1adee2296f6
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-03-17 18:58:32 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,25 @@
-﻿using SwordDamage;
-using System;
+﻿using System;
 
-namespace MyApp
+namespace SwordDamage_Console_Part_1
 {
-    internal class SwordDamage
+    class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Random random = new Random();
-            SwordArt instance = new SwordArt();
+            SwordDamage swordDamage = new SwordDamage();
             while (true)
             {
                 Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, " +
-                    "3 for both, anything else to quit: ");
+                               "3 for both, anything else to quit: ");
                 char key = Console.ReadKey().KeyChar;
-                if (key != '0' && key != '1' && key != '2' && key != '3')
-                {
-                    return;
-                } 
-                int roll=random.Next(1,7)+random.Next(1,7)+ random.Next(1,7);
+                if (key != '0' && key != '1' && key != '2' && key != '3') return;
+                int roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+                swordDamage.Roll = roll;
+                swordDamage.SetMagic(key == '1' || key == '3');
+                swordDamage.SetFlaming(key == '2' || key == '3');
+                Console.WriteLine("\nRolled " + roll + " for " + swordDamage.Damage + " HP\n");
             }
-
         }
     }
 }
--- SwordArt.cs
+++ SwordArt.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace SwordDamage
+namespace SwordDamage_Console_Part_1
 {
-    internal class SwordArt
+    class SwordDamage
     {
-        Random random = new Random();
-        SwordArt instance= new SwordArt();
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
 
+        public int Roll;
+        public decimal MagicMultiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
     }
 }
