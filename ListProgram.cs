a09569d60c3a1812cbb132a1362faad3d270ad5f
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-03-31 22:08:52 +0300

    Add files via upload

---
+++ ListProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CustomList
+{
+    internal class ListProgram
+    {
+        static void Main(string[] args)
+        {
+            List list = new List();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(40);
+            list.Add(50);
+            list.Add(60);
+            list.RemoveAt(0);
+            list.RemoveAt(1);
+            list.RemoveAt(2);
+            list.Contains(10);
+            list.Contains(20);
+            
+            for (int i = 0;i<list.Count;i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
