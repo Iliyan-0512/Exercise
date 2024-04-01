5f5f8a01a4f3304f116d16193de5af747f360617
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-04-01 18:48:13 +0300

    Add files via upload

---
+++ TanksTeleric.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command=Console.ReadLine().ToUpper();
+            int x = 0;
+            int y = 0;
+            for (int i = 1; i < command.Count(); i++)
+            {
+                char current = command[i];
+                if (current == 'R')
+                {
+                    x += 1;
+                }
+                else if (current == 'L')
+                {
+                    x -= 1;
+                }
+                else if (current == 'D')
+                {
+                    y -= 1;
+                }
+                else if (current == 'U')
+                {
+                    y += 1;
+                }
+            }
+            Console.WriteLine($"{x}, {y}");
+
+        }
+    }
+}
