10116edffa51dd8af41b021d2aedbf4fcdb70105
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-04-10 23:33:41 +0300

    Add files via upload
    dont ready. I have to fix output

---
+++ DataStore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+   class DataStore<T>
+    {
+        public T Data { get; set; }
+        //public T data;
+        public T[] data=new T[10];
+        public void AddOrUpdate(int index, T item)
+        {
+            if (index>=0 && index<10)
+            {
+                data[index]=item;
+            }
+        }
+        public T GetData(int index)
+        {
+            if(index>=0 && index<10)
+            {
+                return data[index];
+            }
+            else
+            {
+                return default(T);  
+            }
+        }
+    }
+}
---
+++ Generic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GenericDemo
+{
+    public static  class ArrayCreator
+    {
+        static void Main(string[] args)
+        {
+          DataStore<string> store=new DataStore<string>();
+            store.Data = "Hello World";
+            DataStore<int> store2 = new DataStore<int>();
+            store2.Data = 100;
+            Console.WriteLine(store);
+            Console.WriteLine(store2);
+
+            DataStore<string> cities= new DataStore<string>();
+            cities.AddOrUpdate(0,"Mumbai");
+            cities.AddOrUpdate(1, "Chicago");
+            cities.AddOrUpdate(2, "London");
+           
+            
+
+            DataStore<int> empIds=new DataStore<int>();
+            empIds.AddOrUpdate(0,50);
+            empIds.AddOrUpdate(1, 65);
+            empIds.AddOrUpdate(2, 89);
+           
+            Console.WriteLine(cities);
+            Console.WriteLine(empIds);
+            Console.ReadKey();
+        }
+    }
+}
