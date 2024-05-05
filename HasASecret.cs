7871d276a11c5938bbf7be396f45ed257c728c1c
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-05-05 22:02:53 +0300

    Add files via upload

---
+++ HasASecret.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+namespace MyApp
+{
+    internal class HasASecret
+    {
+        static void Main(string[] args)
+        {
+            HasASecrets keeper=new HasASecrets();
+            FieldInfo[] fields=keeper.GetType().GetFields( BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (FieldInfo field in  fields)
+            {
+                Console.WriteLine(field.GetValue(keeper));
+            }
+        }
+    }
+    class HasASecrets
+    {
+        private string secret = "xyzzy";
+    }
+}
