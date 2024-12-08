7e5d5cd53b30c939caded5aa3005d67871e1e793
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2024-12-08 15:56:22 +0200

    Add files via upload

---
+++ exs.cpp
@@ -0,0 +1,46 @@
+﻿using namespace std;
+
+#include <iostream>;
+#include <iomanip>;
+
+int main()
+{
+	/*for (int row = 1; row <= 7; row++)
+	{
+		for (int column = 1; column <= row; column++)
+		{
+			cout << column;
+		}
+		cout <<endl;
+	}*/
+
+	/*for (int i = 7; i >= 1; i--)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout << j<<" ";
+		}
+		cout << endl;
+	}*/
+
+	int n,currentSum=0,firstN=1,secondN=2;
+	cin >> n;
+	do
+	{
+		cin >> n;
+	} while (n <= 0);
+	if (n==1 || n==2)
+	{
+		cout << 1;
+	}
+	for (int i = 3; i <= n; i++)
+	{
+		currentSum = firstN + secondN;
+		firstN = secondN;
+		secondN = currentSum;
+	}
+	cout << currentSum;
+	return 0;
+}
+
+
