a2b5c98050340aedb32813c48d14f6eda8e95936
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2025-01-30 23:28:40 +0200

    EntitSuccessfully

--- Program.cs
+++ Program.cs
@@ -1,42 +1,10 @@
-﻿using System;
-using System.Linq;
+﻿using entitiFrameworkWithMysql.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace MyApp
+public class ApplicationDbContext : DbContext
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
-            int[,] matrix = new int[sizes[0], sizes[1]];
-            int sumEvenOnOddRows = 0;
-            int sumOddOnEvenCols = 0;
-            for (int row = 0; row < matrix.GetLength(0); row++)
-            {
-                int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-                for (int col = 0; col < matrix.GetLength(1); col++)
-                {
-                    matrix[row, col] = input[col];
-                }
-            }
+    public DbSet<Member> Members { get; set; }
 
-            for (int row = 0; row < matrix.GetLength(0); row++) 
-            {
-                for (int col = 0; col < matrix.GetLength(1); col++) 
-                {
-                    if ((row + 1) % 2 != 0 && matrix[row, col] % 2 == 0)
-                    {
-                        sumEvenOnOddRows += matrix[row, col];
-                    }
-                    if ((row + 1) % 2 == 0 && matrix[row, col] % 2 != 0)
-                    {
-                        sumOddOnEvenCols += matrix[row, col];
-                    }
-                }
-            }
-
-            Console.WriteLine(sumEvenOnOddRows);
-            Console.WriteLine(sumOddOnEvenCols);
-        }
-    }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
 }
